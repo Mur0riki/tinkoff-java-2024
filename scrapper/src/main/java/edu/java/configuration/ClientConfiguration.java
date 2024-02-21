d6c70b391e923d57c5ef84b4c55d0a91ecbad0a8
Mur0riki <nevzlomaite2283@mail.ru>
2024-02-21 15:24:41 +0300

    edit pom's(wiremock-standalone) Exception: add ApiErrorException Configuration: edit ApplicationConfig add ClientConfiguration Clients: add GithubClient add StackOverflowClient DTO: add GithubRepositoryResponse add StackOverflowQuestionResponse Scheduler: add LinkUpdaterScheduler Tests: add StackOverflowClientTest

---
+++ scrapper/src/main/java/edu/java/configuration/ClientConfiguration.java
@@ -0,0 +1,27 @@
+package edu.java.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class ClientConfiguration {
+    private static final String CONTENT_TYPE = "Content-Type";
+
+    @Bean
+    public WebClient stackOverFlowWebClient(ApplicationConfig applicationConfig) {
+        return WebClient.builder()
+            .baseUrl(applicationConfig.baseStackOverflowUrl())
+            .defaultHeader(CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .build();
+    }
+
+    @Bean
+    public WebClient githubWebClient(ApplicationConfig applicationConfig) {
+        return WebClient.builder()
+            .baseUrl(applicationConfig.baseGithubUrl())
+            .defaultHeader(CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .build();
+    }
+}
