6c3f57882d47cc7a3bfd951f89968a592116a82c
Mur0riki <nevzlomaite2283@mail.ru>
2024-02-26 19:23:21 +0300

    Edit ApplicationConfig

--- scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
@@ -3,18 +3,16 @@ package edu.java.configuration;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.time.Duration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
 public record ApplicationConfig(
     @NotEmpty
-    @DefaultValue("app.property: ${baseGitHubUrl}")
-    String baseGithubUrl,
+    String baseGitHubUrl,
     @NotEmpty
-    @DefaultValue("app.property: ${baseStackOverflowUrl}")
     String baseStackOverflowUrl,
     @NotNull
     Scheduler scheduler
