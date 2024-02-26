ce6802b51c44bde510153f9b92c9591c1114f1b3
Mur0riki <nevzlomaite2283@mail.ru>
2024-02-26 19:37:33 +0300

    Import again?

--- scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
@@ -3,7 +3,6 @@ package edu.java.configuration;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.time.Duration;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
